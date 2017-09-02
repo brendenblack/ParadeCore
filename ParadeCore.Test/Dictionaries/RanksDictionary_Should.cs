5bf9cb588d36af69452f01a6001ba3c5e2b6ce52
Brenden Black <brenden.black@gov.bc.ca>
2017-09-02 16:13:22 -0700

    Added rank dictionary and Member, tests for both

---
+++ ParadeCore.Test/Dictionaries/RanksDictionary_Should.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ParadeCore.Test.Dictionaries
+{
+    public class RanksDictionary_Should
+    {
+        [Theory]
+        [InlineData(Rank.Private, RankModifier.Standard, "Private", "Pte")]
+        public void ReturnExpectedValuesWhenAllParametersProvided(Rank rank, RankModifier modifier, string expectedName, string expectedAbbreviation)
+        {
+            var result = ParadeCore.Dictionaries.RankDictionary.Lookup(rank, modifier);
+
+            Assert.True(result.name.Equals(expectedName));
+            Assert.True(result.abbreviation.Equals(expectedAbbreviation));
+        }
+
+        [Theory]
+        [InlineData(Rank.PrivateRecruit, "Private (Recruit)", "Pte(R)")]
+        [InlineData(Rank.PrivateBasic, "Private (Basic)", "Pte(B)")]
+        [InlineData(Rank.Private, "Private", "Pte")]
+        [InlineData(Rank.Corporal, "Corporal", "Cpl")]
+        [InlineData(Rank.MasterCorporal, "Master Corporal", "MCpl")]
+        [InlineData(Rank.Sergeant, "Sergeant", "Sgt")]
+        [InlineData(Rank.WarrantOfficer, "Warrant Officer", "WO")]
+        [InlineData(Rank.MasterWarrantOfficer, "Master Warrant Officer", "MWO")]
+        [InlineData(Rank.ChiefWarrantOfficer, "Chief Warrant Officer", "CWO")]
+        public void ReturnStandardNcoRanksWhenNoModifierProvided(Rank rank, string expectedName, string expectedAbbreviation)
+        {
+            var result = ParadeCore.Dictionaries.RankDictionary.Lookup(rank);
+
+            Assert.True(result.name.Equals(expectedName));
+            Assert.True(result.abbreviation.Equals(expectedAbbreviation));
+        }
+
+        [Theory]
+        [InlineData(Rank.PrivateRecruit, RankModifier.Navy, "Ordinary Seaman (Recruit)", "OS(R)")]
+        [InlineData(Rank.PrivateBasic, RankModifier.Navy, "Ordinary Seaman (Basic)", "OS(B)")]
+        [InlineData(Rank.Private, RankModifier.Navy, "Ordinary Seaman", "OS")]
+        [InlineData(Rank.Corporal, RankModifier.Navy, "Able Seaman", "AS")]
+        [InlineData(Rank.MasterCorporal, RankModifier.Navy, "Master Seaman", "MS")]
+        [InlineData(Rank.Sergeant, RankModifier.Navy, "Petty Officer 2nd Class", "PO2")]
+        [InlineData(Rank.WarrantOfficer, RankModifier.Navy, "Petty Officer 1st Class", "PO1")]
+        [InlineData(Rank.MasterWarrantOfficer, RankModifier.Navy, "Chief Petty Officer 2nd Class", "CPO2")]
+        [InlineData(Rank.ChiefWarrantOfficer, RankModifier.Navy, "Chief Petty Officer 1st Class", "CPO1")]
+        public void ReturnExpectedNavalNcoRanks(Rank rank, RankModifier modifier, string expectedName, string expectedAbbreviation)
+        {
+            var result = ParadeCore.Dictionaries.RankDictionary.Lookup(rank, modifier);
+
+            Assert.True(result.name.Equals(expectedName));
+            Assert.True(result.abbreviation.Equals(expectedAbbreviation));
+        }
+
+        [Theory]
+        [InlineData(Rank.PrivateRecruit, RankModifier.AirForce, "Aviator (Recruit)", "Avr(R)")]
+        [InlineData(Rank.PrivateBasic, RankModifier.AirForce, "Aviator (Basic)", "Avr(B)")]
+        [InlineData(Rank.Private, RankModifier.AirForce, "Aviator", "Avr")]
+        public void ReturnExpectedAirForceNcoRanks(Rank rank, RankModifier modifier, string expectedName, string expectedAbbreviation)
+        {
+            var result = ParadeCore.Dictionaries.RankDictionary.Lookup(rank, modifier);
+
+            Assert.True(result.name.Equals(expectedName));
+            Assert.True(result.abbreviation.Equals(expectedAbbreviation));
+        }
+    }
+}
---
+++ ParadeCore.Test/Member_Should.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ParadeCore.Domain;
+
+namespace ParadeCore.Test
+{
+    public class Member_Should
+    {
+        [Theory]
+        [InlineData("S12345678")]
+        [InlineData("b87654321")]
+        public void SetServiceNumberWhenValid(string serviceNumber)
+        {
+            var member = new Member(serviceNumber);
+
+            Assert.True(member.ServiceNumber.Equals(serviceNumber.ToUpper()));
+        }
+
+        [Theory]
+        [InlineData("111209")]
+        [InlineData("kljawetalkjsf")]
+        [InlineData("1A2345678")]
+        [InlineData("A12 345 678")]
+        public void ThrowsWhenServiceNumberIsInvalid(string serviceNumber)
+        {
+            Assert.Throws<ArgumentException>(() => new Member(serviceNumber));
+        }
+        
+        [Fact]
+        public void ThrowsWhenServiceNumberIsNullOrWhitespace()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Member(""));
+            Assert.Throws<ArgumentNullException>(() => new Member(null));
+        }
+    }
+}
